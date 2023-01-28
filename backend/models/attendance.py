566db8ba8df835592d25ebe5778abc23954b623f
Anees Alawmleh <jezar1231@gmail.com>
2023-01-28 13:28:35 -0600

    Check

--- backend/models/attendance.py
+++ backend/models/attendance.py
@@ -6,6 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 # attendance class that returnrs the attendance, customer, and event IDs
+# Test
 class attendance(db.Model):
     attendance_id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, primary_key=True)

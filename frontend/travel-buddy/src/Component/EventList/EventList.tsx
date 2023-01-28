63eb37a0fcda3d1d60f458e8e450636beb4b10ea
Alonso Nunez <alonso@Alonsos-MacBook-Air.local>
2023-01-28 13:59:27 -0600

    Stash changes

--- frontend/travel-buddy/src/Component/EventList/EventList.tsx
+++ frontend/travel-buddy/src/Component/EventList/EventList.tsx
@@ -1,61 +1,55 @@
 import { getEventListeners } from "events";
 import React, { useState, useEffect } from "react";
 import { setConstantValue } from "typescript";
-import  {eventsAPI}  from "../../API/eventsAPI";
+import { eventsAPI } from "../../API/eventsAPI";
 import { eventModel } from "../../Models/eventModel";
 
+type EventModelType = {
+  eventData: eventModel;
+  checkValue: boolean;
+};
+
 function EventList() {
-  const [eventCheckList, setEventCheckList] = useState<{eventData: eventModel, checkValue: boolean}(
-  
-    // {
-    //   itemText: "Concert",
-    //   checkValue: false,
-    // },
-    // {
-    //   itemText: "Dinner",
-    //   checkValue: false,
-    // },
-    // {
-    //   itemText: "Expo",
-    //   checkValue: false,
-    // },
-  );
-  useEffect(()=> {
-//when page loads we do this function
-eventsAPI.getEventList().then(response => {setEventCheckList(response.data)
-console.log(response)});
-
-
-  }, [])
+  const [eventCheckList, setEventCheckList] =
+    useState<Array<{ eventData: eventModel; checkValue: boolean }>>();
+  useEffect(() => {
+    console.log(eventsAPI);
+    //when page loads we do this function
+    eventsAPI.getEventList().then((response) => {
+      setEventCheckList(response.data);
+      console.log(response);
+    })
+    .catch((e) => console.log(e));
+  }, []);
   function handleCheckbox(e: any) {
     //function that handles checkbox inputs
-    if(eventCheckList !== undefined)
+    if (eventCheckList !== undefined)
       setEventCheckList(
         eventCheckList.map((x) => {
-        if (x.itemText === e.target.id) {
-          x.checkValue = e.target.checked; //reasssings value to check if its true
-        }
-        return x;
-      })
-    );
+          if (x.eventData.name === e.target.id) {
+            x.checkValue = e.target.checked; //reasssings value to check if its true
+          }
+          return x;
+        })
+      );
   }
 
   return (
     <div className="eventList">
       Event List
       <ul>
-        {eventCheckList.map((x) => (
+        {eventCheckList?.map((x) => (
           <li>
             {" "}
             <input
-              id={x.itemText}
+              id={x.eventData.name}
               type="checkbox"
               checked={x.checkValue}
               onClick={(e) => {
                 handleCheckbox(e); //calling the functiion when clicked passes "e(event)"
               }}
             />
-            <span>{x.itemText}</span>
+            <span>{x.eventData.name}</span>
           </li>
         ))}
       </ul>

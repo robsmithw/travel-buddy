8304bce363c467faa55d45a73d91393f2fa9f837
Alonso Nunez <alonso@Alonsos-MacBook-Air.local>
2023-01-28 17:15:20 -0600

    added a radio btn && added event description

--- frontend/travel-buddy/src/Component/EventDetails/EventDetails.tsx
+++ frontend/travel-buddy/src/Component/EventDetails/EventDetails.tsx
@@ -1,15 +1,14 @@
 import React ,{useState, useEffect} from 'react'
 import { eventModel } from '../../Models/eventModel'
 
-function EventDetails() {
-  const[EventDetails, setEventDetails]= useState<eventModel>({category: 'concert', date: "11/27/22", name: "the_smiths", time: "11:30am"})
+function EventDetails(props: { EventDetails: eventModel }) {
   return (
     <div className="eventDetails">
       <h1> Event Details</h1>
-      <p> {EventDetails.category}</p>
-      <p> {EventDetails.date}</p>
-      <p> {EventDetails.name}</p>
-      <p> {EventDetails.time}</p>
+      <p> {props.EventDetails.category}</p>
+      <p> {props.EventDetails.date}</p>
+      <p> {props.EventDetails.name}</p>
+      <p> {props.EventDetails.time}</p>
     </div>
   )
 }

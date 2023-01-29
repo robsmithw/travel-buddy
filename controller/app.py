ac422d1c00c6501c036748441d1835fd6d9bde6a
Bryan <bryanhernandez3233@gmail.com>
2023-01-28 19:54:57 -0600

    stashing changes for itenerary

--- controller/app.py
+++ controller/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from datetime import datetime
+import sqlite3
 import requests
 app = Flask(__name__)
 
@@ -32,3 +33,15 @@ def filter_events():
     response = requests.request("GET", url, headers=headers, data=payload)
 
     return response.json()
+
+@app.route("/save_events",methods = ['POST'])
+def save_events():
+    event_form = request.form['event_form']
+    # assume event form passes userId and many eventIds (event ids in an array)
+    sqliteConnection = sqlite3.connect('backend/travel.sqlite3')
+    cursor = sqliteConnection.cursor()
+    sqlite_insert_query = """INSERT INTO Attendance (customer_id, event_id) VALUES ({user_id}, {event_id})"""
+    cursor.execute(sqlite_insert_query)
+    cursor.close()
+    sqliteConnection.close()
+    return

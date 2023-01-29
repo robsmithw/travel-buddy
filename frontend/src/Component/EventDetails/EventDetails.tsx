db44a011540030c03415f0057cfa29073c8e31a0
Alonso Nunez <alonso@Alonsos-MacBook-Air.local>
2023-01-29 09:25:52 -0600

    added details to event details

--- frontend/src/Component/EventDetails/EventDetails.tsx
+++ frontend/src/Component/EventDetails/EventDetails.tsx
@@ -3,25 +3,36 @@ import { eventModel } from "../../Models/eventModel";
 
 function EventDetails(props: { EventDetails: eventModel }) {
   return (
-    <div className="border border-succes p-2 mt-2">
+    <div className="border border-succes p-1 mt-2">
       <div className="eventDetails">
         <h1> Event Details</h1>
 
         <div className="row">
           <div className="col">
+            <p>Name:</p>
             <p> {props.EventDetails.title}</p>
+            <p>Description:</p>
+            <p> {props.EventDetails.description}</p>
+            <p>Public:</p>
+            <p> {props.EventDetails.private}</p>
           </div>
           <div className="col">
             <div className="row">
+            <p>Event Start:</p>
               <p> {props.EventDetails.start}</p>
             </div>
             <div className="row">
+            <p>Event End:</p>
               <p> {props.EventDetails.end}</p>
+              <p>Category:</p>
+          <p> {props.EventDetails.category}</p> 
+          <p>Timezone:</p>
+          <p> {props.EventDetails.timezone}</p>
             </div>
           </div>
         </div>
         <div className="row">
-          <p> {props.EventDetails.category}</p>
+
         </div>
       </div>
     </div>
